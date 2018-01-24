227bedd5159144d9a1d9850adaa1c50f5dc35497
esthersweon <esther.s.weon@gmail.com>
2018-01-24 14:58:00 -0800

    Add JQuery and Bootstrap to index.html; beef up Twitter.js component

--- src/Twitter.js
+++ src/Twitter.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './Twitter.css';
-import $ from 'jquery';
 
-const tweetsFromServer = [
-  {
-    "author": "Michael Scott",
-    "text": "Would I rather be feared or loved? Easy, both. I want people to be afraid of how much they love me."
-  },
-  {
-    "author": "Jeff Bezos",
-    "text": "In the end, we are our choices."
-  }
-];
+// const tweetsFromServer = [
+//   {
+//     "author": "Michael Scott",
+//     "text": "Would I rather be feared or loved? Easy, both. I want people to be afraid of how much they love me."
+//   },
+//   {
+//     "author": "Jeff Bezos",
+//     "text": "In the end, we are our choices."
+//   }
+// ];
 
 class Twitter extends Component {
   constructor() {
@@ -20,7 +19,15 @@ class Twitter extends Component {
   render() {
     return (
       <div className="twitter">
-        <h1>Welcome to Twitter</h1>
+        <header className="jumbotron">
+          <div className="container">
+            <h1>See what's happening right now.</h1>
+            <h3>Find community, conversation, and inspiration about the things you love.</h3>
+          </div>
+        </header>
+        <div className="container">
+          <h1>Twitter</h1>
+        </div>
       </div>
     );
   }
